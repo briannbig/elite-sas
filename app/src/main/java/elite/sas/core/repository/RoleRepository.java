029b62477a664d38577bb6f8964a72429b342f86
Brian Barasa <briannbig@gmail.com>
2023-04-13 13:08:03 +0300

    minor change: rename parameter appropriately

--- app/src/main/java/elite/sas/core/repository/RoleRepository.java
+++ app/src/main/java/elite/sas/core/repository/RoleRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
-    Optional<Role> findByRoleName(RoleName companyAdmin);
+    Optional<Role> findByRoleName(RoleName roleName);
 }
